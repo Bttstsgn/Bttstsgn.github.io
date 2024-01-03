88472b97a148529be11b8cade53c33b3f51d5be1
Bttstsgn <147134301+Bttstsgn@users.noreply.github.com>
2024-01-03 15:44:48 +0800

    Update dark.js

--- dark.js
+++ dark.js
@@ -1,9 +1,15 @@
+// "icon" id buhii HTML elelmentiig avch "icon" huvisagchid hadgalna
 var icon = document.getElementById("icon");
+// "Icon" elemented onclickiig hadgalj uguh function
 icon.onclick = function(){
+  // element deer dark theme iig uguh
   document.body.classList.toggle("dark-theme");
+  // dark bolson esehiig shalgah
   if(document.body.classList.contains("dark-theme")){
+    // dark theme bolson bol бол dursiig "assest/sun.png" bolgono
     icon.src="assest/sun.png";
   }else{
+    // esreg vyd "assest/moon.png" 
     icon.src="assest/moon.png";
   }
 }
